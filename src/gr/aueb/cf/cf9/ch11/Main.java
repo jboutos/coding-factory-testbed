3e7c263848b3cff7ac33a20ea1eebc2d205aa746
jboutos <215729236+jboutos@users.noreply.github.com>
2025-12-02 15:34:03 +0200

    feat: Added printlns

--- src/gr/aueb/cf/cf9/ch11/Main.java
+++ src/gr/aueb/cf/cf9/ch11/Main.java
@@ -2,6 +2,13 @@ package gr.aueb.cf.cf9.ch11;
 
 public class Main {
 
-    User john = new User(23L, "John", "Boutos");
+    public static void main(String[] args) {
+        User john = new User(23L, "John", "Boutos");
+
+        System.out.println("Id: " + john.getId());
+        System.out.println("Firstname: " + john.getFirstname());
+        System.out.println("Lastname: " + john.getLastname());
+    }
+
 
 }
